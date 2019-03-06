1fd17e3383c202ab2dd5ee33e5ed94899c776df5
Nick Ashley <nick@shley.us>
2019-03-06 22:16:20 +0000

    Create main.py

---
+++ main.py
@@ -0,0 +1,5 @@
+import signal
+
+catchable_sigs = set(signal.Signals) - {signal.SIGKILL, signal.SIGSTOP}
+for sig in catchable_sigs:
+	signal.signal(sig, print)  # Substitute handler of choice for `print`

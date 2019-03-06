242cf05b3ee460f1490f11509c2c8c520d27bb7d
Nick Ashley <nick@shley.us>
2019-03-06 22:20:54 +0000

    Update app.py

--- app.py
+++ app.py
@@ -3,3 +3,6 @@ import signal
 catchable_sigs = set(signal.Signals) - {signal.SIGKILL, signal.SIGSTOP}
 for sig in catchable_sigs:
 	signal.signal(sig, print)  # Substitute handler of choice for `print`
+
+while True:
+	pass
